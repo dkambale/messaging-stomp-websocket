79e9c4ccfab1a46dc0670f3abfc66cfe942d6891
Dadasaheb <dadasaheb.gopinath@healthfidelity.com>
2022-03-06 14:09:21 +0530

    initial

---
+++ src/main/java/com/example/messagingstompwebsocket/controller/GreetingController.java
@@ -0,0 +1,26 @@
+package com.example.messagingstompwebsocket.controller;
+import com.example.messagingstompwebsocket.bean.Greeting;
+import com.example.messagingstompwebsocket.bean.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+  @MessageMapping("/hello")
+  @SendTo("/topic/greetings1")
+  public Greeting greeting(HelloMessage message) throws Exception {
+    Thread.sleep(1000); // simulated delay
+    return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+  }
+
+  @MessageMapping("/hello1")
+  @SendTo("/topic/greetings")
+  public Greeting greeting1(HelloMessage message) throws Exception {
+    Thread.sleep(1000); // simulated delay
+    return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+  }
+
+}
---
+++ src/main/resources/static/app.js
@@ -0,0 +1,63 @@
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html("");
+}
+
+function connect() {
+    var socket = new SockJS('/gs-guide-websocket');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/greetings', function (greeting) {
+            showGreeting(JSON.parse(greeting.body).content);
+        });
+
+         stompClient.subscribe('/topic/greetings1', function (greeting) {
+                    showGreeting1(JSON.parse(greeting.body).content);
+                });
+
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendName() {
+    stompClient.send("/app/hello", {}, JSON.stringify({'name': $("#name").val()}));
+}
+
+function sendName1() {
+    stompClient.send("/app/hello1", {}, JSON.stringify({'name': $("#name1").val()}));
+}
+
+function showGreeting(message) {
+    $("#greetings1").append("<tr><td>" + message + "</td></tr>");
+}
+function showGreeting1(message) {
+    $("#greetings").append("<tr><td>" + message + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+    $( "#send" ).click(function() { sendName(); });
+    $( "#send1" ).click(function() { sendName1(); });
+});
